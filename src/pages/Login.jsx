d8eff859bcc7423d34a0b191fdd0cab581e41515
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-05-24 13:06:17 +0530

    Fixed import issue

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import bgImage from '../assets/background.png';
+import bgImage from '../assets/Background.png';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase'; 
 import { useNavigate } from 'react-router-dom';
